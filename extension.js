6c2f09da4a411b33320db35e5e9870c0eace444d
dileepadev <251038784+dileepadev@users.noreply.github.com>
2026-06-03 20:22:37 +0530

    fix: Udate SPDX-License-Identifier

--- extension.js
+++ extension.js
@@ -13,7 +13,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- * SPDX-License-Identifier: GPL-2.0-or-later
+ * SPDX-License-Identifier: MIT
  */
 import {Extension} from 'resource:///org/gnome/shell/extensions/extension.js';
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
